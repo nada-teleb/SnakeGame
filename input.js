6c4187cf86baf025a738add81aa6eaadf8fa693d
nada_teleb <telebnada@gmail.com>
2024-06-22 14:17:21 +0300

    Fix keyboard movements bug to comply to snake game movement limitations; can't go up if last move was down, and can't go right if last move was left, and vice versa.

--- input.js
+++ input.js
@@ -1,23 +1,39 @@
 let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
 
 // modify input direction whenever a key is hit
 window.addEventListener('keydown', e => {
 	switch (e.key) {
 		case 'ArrowUp':
+			if (lastInputDirection.y !== 0) break;
+
 			inputDirection = { x: 0, y: -1 };
 			break;
+
 		case 'ArrowDown':
+			if (lastInputDirection.y !== 0) break;
+
 			inputDirection = { x: 0, y: 1 };
 			break;
+
 		case 'ArrowLeft':
+			if (lastInputDirection.x !== 0) break;
+
 			inputDirection = { x: -1, y: 0 };
 			break;
+
 		case 'ArrowRight':
+			if (lastInputDirection.x !== 0) break;
+
 			inputDirection = { x: 1, y: 0 };
 			break;
 	}
 })
 
 export function getInputDirection() {
+
+	// store last / previous input direction
+	lastInputDirection = inputDirection;
+
 	return inputDirection;
 }
