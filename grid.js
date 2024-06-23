b7f94bf4f251e409357aee3b87d8e88576a4d729
nada_teleb <telebnada@gmail.com>
2024-06-23 21:04:03 +0300

    Add outsideGrid function to check if any position is outside of game board grid.

--- grid.js
+++ grid.js
@@ -6,3 +6,11 @@ export function randomGridPosition() {
 		y: Math.floor(Math.random() * GRID_SIZE) + 1 
 	};
 }
+
+// check if position is greater than 21 (grid size) or less than 1 (minimum grid size)
+export function outsideGrid(position) {
+	return (
+		position.x < 1 || position.x > GRID_SIZE || 
+		position.y < 1 || position.y > GRID_SIZE
+	);
+}
